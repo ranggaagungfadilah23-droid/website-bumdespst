32d0152fb73a93a28db2957ecd3e47871fac22be
ranggaagungfadilah23-droid <ranggaagungfadilah23@gmail.com>
2026-06-17 20:56:21 +0700

    yayaya

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,15 +1,17 @@
 import Echo from 'laravel-echo';
-
 import Pusher from 'pusher-js';
+
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: "pusher",
+    broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true,
-    wsHost: import.meta.env.VITE_PUSHER_HOST,
-    wsPort: import.meta.env.VITE_PUSHER_PORT,
-    wssPort: import.meta.env.VITE_PUSHER_PORT,
-    enabledTransports: ["ws", "wss"],
+    authEndpoint: '/broadcasting/auth', // ← tambahkan ini
+    auth: {
+        headers: {
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')?.getAttribute('content'),
+        }
+    }
 });
